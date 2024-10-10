0e6fafe8f43be2fc279a6068aa65e1b48653b61a
xKiimbb <xkiimbbmusic@gmail.com>
2024-10-10 21:25:40 +0200

    Opdracht-2-Cond-Stat-Opgelost

---
+++ 3-conditionele-statements/opdracht-2/Opdracht-2-Cond-Stat.js
@@ -0,0 +1,16 @@
+// 1) Schrijf een functie genaamd maximum.
+// 2) De functie accepteert drie parameters, a, b en c.
+// 3) De functie geeft het grootste getal van de drie terug.
+// 4) Test de functie met de getallen 10, 20 en 15.
+
+function maximum(a, b, c) {
+  if ((a > b) & c) {
+    return a;
+  } else if ((b > a, c)) {
+    return b;
+  } else {
+    return c;
+  }
+}
+
+console.log(maximum(10, 20, 15));
