25f2859653cdd43f5435e845627a1a49f74dc3cd
unknown <xkiimbbmusic@gmail.com>
2024-10-10 19:35:57 +0200

    Opdracht-1-Functies

---
+++ 2-functies/opdracht-1/Opdracht-1-Functies.js
@@ -0,0 +1,12 @@
+// Maak een javascript bestand aan genaamd Opdracht-1.js, maak hier de volgende oefening in en probeer van iedere opdracht een commit te maken en deze te pushen naar de repo
+
+// 1) Schrijf een functie genaamd vermenigvuldig.
+// 2) De functie accepteert twee parameters, x en y.
+// 3) De functie geeft het product van x en y terug.
+// 4) Roep de functie aan met de waarden 5 en 7 en print het resultaat.
+
+function vermenigvuldig(x, y) {
+  return x * y;
+}
+
+console.log(vermenigvuldig(5, 7));
