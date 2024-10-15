f07fdb08cfefbefd2f5f5ae6d3f8097793964a9c
xKiimbb <xkiimbbmusic@gmail.com>
2024-10-15 19:32:22 +0200

    Opdracht-1-Lussen-Arrays-Opgelost

---
+++ 4-lussen-arrays/opdracht-1/Opdracht-1-Lussen-Arrays.js
@@ -0,0 +1,8 @@
+// 1) Maak een array genaamd getallen met de waarden [1, 2, 3, 4, 5].
+// 2) Schrijf een for-lus die elk element van de array vermenigvuldigt met 2 en het resultaat uitprint.
+
+let getallen = [1, 2, 3, 4, 5];
+for (let i = 0; i < getallen.lenght; i++) {
+  getallen[i] * 2;
+  console.log(i);
+}
