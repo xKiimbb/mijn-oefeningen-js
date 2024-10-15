08c5aea80243105295ede27fda2d65a6a5955a21
xKiimbb <xkiimbbmusic@gmail.com>
2024-10-15 19:38:13 +0200

    Opdracht-1-Lussen-Arrays-Opgelost

--- 4-lussen-arrays/opdracht-1/Opdracht-1-Lussen-Arrays.js
+++ 4-lussen-arrays/opdracht-1/Opdracht-1-Lussen-Arrays.js
@@ -2,7 +2,8 @@
 // 2) Schrijf een for-lus die elk element van de array vermenigvuldigt met 2 en het resultaat uitprint.
 
 let getallen = [1, 2, 3, 4, 5];
-for (let i = 0; i < getallen.lenght; i++) {
-  getallen[i] * 2;
-  console.log(i);
+for (let i = 0; i < getallen.length; i++) {
+  let resultaat = getallen[i] * 2;
+
+  console.log(resultaat);
 }
---
+++ 4-lussen-arrays/opdracht-2/Opdracht-2-Lussen-Arrays.js
@@ -0,0 +1,4 @@
+// 1) Schrijf een functie genaamd telOp.
+// 2) De functie accepteert een array van getallen.
+// 3) De functie geeft de som van alle getallen in de array terug.
+// 4) Test de functie met de array [5, 10, 15] en print het resultaat.
