eb4ca982c4135b024f1045ab4a461b4cbaa5ec77
xKiimbb <xkiimbbmusic@gmail.com>
2024-10-15 21:50:55 +0200

    Opdracht-2-Lussen-Arrays-Opgelost

--- 4-lussen-arrays/opdracht-2/Opdracht-2-Lussen-Arrays.js
+++ 4-lussen-arrays/opdracht-2/Opdracht-2-Lussen-Arrays.js
@@ -2,3 +2,13 @@
 // 2) De functie accepteert een array van getallen.
 // 3) De functie geeft de som van alle getallen in de array terug.
 // 4) Test de functie met de array [5, 10, 15] en print het resultaat.
+
+function telOp(array) {
+  let Totaal = 0;
+  for (let i = 0; i < array.length; i++) {
+    Totaal += array[i];
+  }
+  return Totaal;
+}
+
+console.log(telOp([5, 10, 15]));
